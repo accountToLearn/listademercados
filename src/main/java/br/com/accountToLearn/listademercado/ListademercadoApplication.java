ed4e30e310ef9507727341361e7e4c98b83d3beb
accountToLearn <listademercado@hotmail.com>
2021-01-18 15:44:43 -0300

    Iniciando projeto web service listademercados

---
+++ src/main/java/br/com/accountToLearn/listademercado/ListademercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.accountToLearn.listademercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListademercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListademercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/accountToLearn/listademercado/ListademercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accountToLearn.listademercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListademercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
